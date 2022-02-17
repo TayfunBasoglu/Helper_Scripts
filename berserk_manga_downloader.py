6712a53e9f4dbc69a218d584c9e4052dfcf36b66
Tayfun Başoğlu <91539164+TayfunBasoglu@users.noreply.github.com>
2022-02-17 11:48:23 +0300

    Create berserk_manga_downloader.py

---
+++ berserk_manga_downloader.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+from PIL import Image
+
+#chapter url
+page_url = "https://readberserk.com/chapter/berserk-chapter-a0/"
+
+#Create Directory and Go
+os.mkdir("downloadfoldercreateee")
+os.chdir("downloadfoldercreateee")
+
+#Connect and Get Images
+source = BeautifulSoup(requests.get(page_url).content,"lxml")
+images_list = []
+for i in source.find_all("img",attrs={"class":"pages__img"}):
+  images_list.append(i["src"])
+  
+#Download Images
+value = 1
+for u in images_list:
+    print("downloading",u,"as",str(value)+".jpg")
+    r = requests.get(u, allow_redirects=True)
+    open(str(value)+".jpg", 'wb').write(r.content)
+    value +=1
+
+#Make pdf_name
+pdf_name = None
+if page_url[page_url[:-2].rfind("/")+1:].endswith("/"):
+    pdf_name = page_url[page_url[:-2].rfind("/")+1:-1]+".pdf"
+else:
+    pdf_name = page_url[page_url[:-2].rfind("/")+1:]+".pdf"
+
+#Sort Images 
+file_list_sorted = sorted(os.listdir().copy(), key=lambda x: int(os.path.splitext(x)[0]))
+
+#Create Pdf
+liste = []
+for i in file_list_sorted[0:]:
+    if str(i).endswith("jpg"):
+        liste.append(Image.open(i).convert("RGB"))
+liste[0].save("../"+str(pdf_name),save_all=True,append_images=liste[1:])
+
+#Remove Files and Directory
+for files in os.listdir():
+    os.remove(files)
+os.chdir("../")
+os.rmdir("downloadfoldercreateee")
