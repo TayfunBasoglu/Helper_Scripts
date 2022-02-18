2e4c3e88f05d91652f26e54d7edc4808676905db
Tayfun Başoğlu <91539164+TayfunBasoglu@users.noreply.github.com>
2022-02-18 16:49:50 +0300

    Create letterboxd_following_check.py

---
+++ letterboxd_following_check.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+##### Profile Username
+username = "tayyfun"
+
+#lists
+following = []
+followers = []
+
+#get follow lists
+def get_users(source,page_name):
+    persons = source.find_all("div",attrs={"class":"person-summary"})
+    for i in persons:
+        if page_name == "following":   
+            following.append(i.find("h3").find("a")["href"])
+        else:
+            followers.append(i.find("h3").find("a")["href"])
+
+#get pages
+def connect_page(page_name):
+    print("Getting",page_name,"list...")
+    page_num = 1
+    while True:
+        url = "https://letterboxd.com/"+str(username)+"/"+page_name+"/page/"+str(page_num)+"/"
+        source = BeautifulSoup(requests.get(url).content,"lxml")
+        if '<a class="next" href="/'+username+'/'+page_name+'/' in str(source):
+            get_users(source,page_name)
+            page_num += 1
+        else:
+            get_users(source,page_name)
+            break
+
+#pages loop
+for page_names in ("following","followers"):
+    connect_page(page_names)
+
+#list
+print("\n\n# List of Who Doesn't Follow You Back")
+for i in set(following) - set(followers):
+    print("  -",i[1:-1])
