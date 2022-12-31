c636527fdfb17626c04a1155d58942e134ebf7f5
Tayfun Başoğlu <91539164+TayfunBasoglu@users.noreply.github.com>
2022-12-31 08:02:55 +0300

    Update berserk_chapter_to_pdf.py

--- berserk_chapter_to_pdf.py
+++ berserk_chapter_to_pdf.py
@@ -1,50 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import time
 from PIL import Image
+import shutil
+
 
 #chapter url
-page_url = "http://berserkmanga.net/manga/berserk-chapter-1/"
+page_url = "https://berserkchapters.com/manga/berserk-chapter-1/"
+
 
 #Create Directory and Go
+if os.path.isdir('downloadfoldercreateee') == True:
+    shutil.rmtree('downloadfoldercreateee')
 os.mkdir("downloadfoldercreateee")
 os.chdir("downloadfoldercreateee")
 
 #Connect and Get Images
+image_names_list = []
 source = BeautifulSoup(requests.get(page_url).content,"lxml")
-images_list = []
-for i in source.find_all("div",attrs={"class":"img_container"}):
-  images_list.append(i.find("img")["src"])
-  
-#Download Images
-value = 1
-for u in images_list:
-    print("downloading",u,"as",str(value)+".jpg")
-    r = requests.get(u, allow_redirects=True)
-    open(str(value)+".jpg", 'wb').write(r.content)
-    value +=1
-
-#Make pdf_name
-pdf_name = None
-if page_url[page_url[:-2].rfind("/")+1:].endswith("/"):
-    pdf_name = page_url[page_url[:-2].rfind("/")+1:-1]+".pdf"
-else:
-    pdf_name = page_url[page_url[:-2].rfind("/")+1:]+".pdf"
-
-#Sort Images 
-file_list_sorted = sorted(os.listdir().copy(), key=lambda x: int(os.path.splitext(x)[0]))
+list_general = str(source).split(" ")
+for i in list_general:
+    if ".jpg" in i or ".jpeg" in i or ".png" in i:
+            if "https://berserkchapters.com/wp-content/uploads/2019/08/cropped-berserkwall.jpg" in i :
+                pass
+            else:
+                url_image = i[i.find('"')+1:i.find('"',5)]
+                img_data = requests.get(url_image).content
+                time.sleep(0.3)
+                file_name = url_image[url_image.rfind("/")+1:]
+                print(file_name,"done")
+                image_names_list.append(file_name)
+                with open(file_name, 'wb') as handler:
+                    handler.write(img_data)
 
 #Create Pdf
-liste = []
+pdf_list = []
 print("Preparing PDF")
-for i in file_list_sorted[0:]:
-    if str(i).endswith("jpg"):
-        liste.append(Image.open(i).convert("RGB"))
-liste[0].save("../"+str(pdf_name),save_all=True,append_images=liste[1:])
-
-#Remove Files and Directory
-for files in os.listdir():
-    os.remove(files)
+for i in image_names_list:
+    pdf_list.append(Image.open(i).convert("RGB"))
+pdf_list[0].save("../"+page_url[page_url.find("berserk",10):-1]+".pdf",save_all=True,append_images=pdf_list[1:])
 os.chdir("../")
-os.rmdir("downloadfoldercreateee")
-print("Done")
+shutil.rmtree('downloadfoldercreateee')
